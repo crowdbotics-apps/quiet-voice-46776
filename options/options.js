df946e4a1141a92139e83b7f1f91d1f0a864b223
Crowdbotics <team@crowdbotics.com>
2024-03-03 21:07:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quiet-voice-46776",
+  url: "https://quiet-voice-46776.botics.co",
+  api: "https://quiet-voice-46776.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
